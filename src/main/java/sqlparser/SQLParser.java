14dca2af76849e29ed7aa79b1c8f34d90b24d122
Mateusz Kaduk <mateusz.kaduk@gmail.com>
2015-05-05 13:00:37 +0200

    Add XMLWriter for OrthoXML output.

--- src/main/java/sqlparser/SQLParser.java
+++ src/main/java/sqlparser/SQLParser.java
@@ -5,18 +5,21 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 
+import javax.xml.stream.XMLStreamException;
+
 import org.forester.phylogeny.PhylogenyNode;
 
+import sbc.orthoxml.io.OrthoXMLWriter;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
-
-import sqlparser.*;
 
 /**
  * A program entry for the SQLtable parser for results from Hieranoid 2
@@ -24,6 +27,7 @@ import sqlparser.*;
  * @author Mateusz Kaduk <mateusz.kaduk@gmail.com>
  *
  */
+@SuppressWarnings("restriction")
 public class SQLParser {
 	//private static final Logger logger = LogManager.getLogger(SQLParser.class);
 	public static PrintWriter openWriter(File outfile) throws FileNotFoundException {
@@ -61,6 +65,12 @@ public class SQLParser {
 				.acceptsAll(Arrays.asList("o", "output"),
 						"A file to write list of pairs of orthologs.")
 				.withRequiredArg().ofType(File.class);
+		
+		OptionSpec<File> xmlFile = parser
+				.acceptsAll(Arrays.asList("x", "orthoxml"),
+						"A file to write orthologs in orthoXML format.")
+				.withRequiredArg().ofType(File.class);
+		
 		try {
 			// Parse command line
 			OptionSet options = parser.parse(args);
@@ -68,11 +78,17 @@ public class SQLParser {
 			// Open files for sequential writes
 			PrintWriter pairWriter = null;
 			PrintWriter listWriter = null;
+			OrthoXMLWriter orthoXMLWriter = null;
+			
 			if(options.has(pairsFile))
 				pairWriter = openWriter(options.valueOf(pairsFile));
 			if(options.has(listFile))
 				listWriter = openWriter(options.valueOf(listFile));
+			if(options.has(xmlFile)) {
+				String timestamp = new SimpleDateFormat("MM-dd-yyyy-HH-mm-ss").format(new Date());
+				orthoXMLWriter = new OrthoXMLWriter(options.valueOf(xmlFile), "Hieranoid2", timestamp);
 			
+			}
 			// Check mandatory arguments
 			if(options.has(treeFile) && options.hasArgument(sqlDir)
 					&& options.has(proDir)) {
@@ -80,8 +96,7 @@ public class SQLParser {
 				List<PhylogenyNode> leafList = treader.getAllLeafs();
 				DirectoryReader sreader = new DirectoryReader(
 						options.valueOf(sqlDir), "sqltable.");
-
-				// ProteomesBank pbank = null;
+				
 				ProteomesBank pbank = new ProteomesBank(options.valueOf(proDir));
 				pbank.loadProteomes();
 				
@@ -100,14 +115,14 @@ public class SQLParser {
 						// Get all nodes prior to LCA
 						List<PhylogenyNode> branchList = treader
 								.getAllBranches(lca);
-
+						
 						// Combine sqltables to process
 						File tmp = File.createTempFile(lca.getName(), ".all");
 
 						ArrayList<File> mergeFiles = new ArrayList<File>();
 						for (PhylogenyNode branch : branchList) {
 							mergeFiles.add(sreader.getFileMatching("sqltable."
-									+ branch.getName() + ".*"));
+									+ branch.getName()));
 						}
 
 						// Combine sqltable files for current LCA
@@ -140,6 +155,13 @@ public class SQLParser {
 					
 							pbuilder.writePairs(pairWriter);
 						}
+						
+						// Save orthologs to XML
+						if (options.has(xmlFile)) {
+							XMLWriter xmlwr = new XMLWriter(pairGroups, pbank.getSpecies(), pset);
+							
+							xmlwr.writeXML(orthoXMLWriter);
+						}
 					}
 				}
 				// Close output files
@@ -147,9 +169,12 @@ public class SQLParser {
 					listWriter.close();
 				if(options.has(pairsFile))
 					pairWriter.close();
+				if(options.has(xmlFile))
+					orthoXMLWriter.close();
+				
 			} else {
 				System.out
-						.println("SQLTableParser 0.1 - Parser converting sqltable files to list of tab separate pairs of orthologs.");
+						.println("SQLTableParser 0.1 - Parser converting sqltable files to orthologs.");
 				System.out
 						.println("Corresponding author - Mateusz Kaduk <mateusz.kaduk@scilifelab.se>\n");
 
@@ -157,6 +182,8 @@ public class SQLParser {
 				parser.printHelpOn(System.out);
 			}
 		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (@SuppressWarnings("restriction") XMLStreamException e) {
 			e.printStackTrace();
 		}
 	}
