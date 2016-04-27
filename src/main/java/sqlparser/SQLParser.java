bb098cd9ea054629a74294d19c66437c93094b91
Mateusz Kaduk <mateusz.kaduk@gmail.com>
2016-04-27 15:54:50 +0200

    Remove OrthoXML dependency, not used.

--- src/main/java/sqlparser/SQLParser.java
+++ src/main/java/sqlparser/SQLParser.java
@@ -5,20 +5,20 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
+
 import java.util.HashSet;
 import java.util.List;
 
-import javax.xml.stream.XMLStreamException;
+//import javax.xml.stream.XMLStreamException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.forester.phylogeny.PhylogenyNode;
 
-import sbc.orthoxml.io.OrthoXMLWriter;
+//import sbc.orthoxml.io.OrthoXMLWriter;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
@@ -29,7 +29,6 @@ import joptsimple.OptionSpec;
  * @author Mateusz Kaduk <mateusz.kaduk@gmail.com>
  *
  */
-@SuppressWarnings("restriction")
 public class SQLParser {
 	private static final Logger logger = LogManager.getLogger(SQLParser.class);
 	
@@ -69,11 +68,11 @@ public class SQLParser {
 						"A file to write list of pairs of orthologs.")
 				.withRequiredArg().ofType(File.class);
 		
-		OptionSpec<File> xmlFile = parser
-				.acceptsAll(Arrays.asList("x", "orthoxml"),
-						"A file to write orthologs in orthoXML format.")
-				.withRequiredArg().ofType(File.class);
-		
+//		OptionSpec<File> xmlFile = parser
+//				.acceptsAll(Arrays.asList("x", "orthoxml"),
+//						"A file to write orthologs in orthoXML format.")
+//				.withRequiredArg().ofType(File.class);
+//		
 		try {
 			// Parse command line
 			OptionSet options = parser.parse(args);
@@ -81,17 +80,17 @@ public class SQLParser {
 			// Open files for sequential writes
 			PrintWriter pairWriter = null;
 			PrintWriter listWriter = null;
-			OrthoXMLWriter orthoXMLWriter = null;
+		//	OrthoXMLWriter orthoXMLWriter = null;
 			
 			if(options.has(pairsFile))
 				pairWriter = openWriter(options.valueOf(pairsFile));
 			if(options.has(listFile))
 				listWriter = openWriter(options.valueOf(listFile));
-			if(options.has(xmlFile)) {
-				String timestamp = new SimpleDateFormat("MM-dd-yyyy-HH-mm-ss").format(new Date());
-				orthoXMLWriter = new OrthoXMLWriter(options.valueOf(xmlFile), "Hieranoid2", timestamp);
-			
-			}
+//			if(options.has(xmlFile)) {
+//				String timestamp = new SimpleDateFormat("MM-dd-yyyy-HH-mm-ss").format(new Date());
+//				orthoXMLWriter = new OrthoXMLWriter(options.valueOf(xmlFile), "Hieranoid2", timestamp);
+//			
+//			}
 			// Check mandatory arguments
 			if(options.has(treeFile) && options.hasArgument(sqlDir)
 					&& options.has(proDir)) {
@@ -166,12 +165,12 @@ public class SQLParser {
 							pbuilder.writePairs(pairWriter);
 						}
 						
-						// Save orthologs to XML
-						if (options.has(xmlFile)) {
-							XMLWriter xmlwr = new XMLWriter(pairGroups, pbank.getSpecies(), pset);
-							
-							xmlwr.writeXML(orthoXMLWriter);
-						}
+//						// Save orthologs to XML
+//						if (options.has(xmlFile)) {
+//							XMLWriter xmlwr = new XMLWriter(pairGroups, pbank.getSpecies(), pset);
+//							
+//							xmlwr.writeXML(orthoXMLWriter);
+//						}
 					}
 				}
 				// Close output files
@@ -179,9 +178,9 @@ public class SQLParser {
 					listWriter.close();
 				if(options.has(pairsFile))
 					pairWriter.close();
-				if(options.has(xmlFile))
-					orthoXMLWriter.close();
-				
+//				if(options.has(xmlFile))
+//					orthoXMLWriter.close();
+//				
 			} else {
 				System.out
 						.println("SQLTableParser 0.1 - Parser converting sqltable files to orthologs.");
@@ -193,8 +192,9 @@ public class SQLParser {
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		} catch (@SuppressWarnings("restriction") XMLStreamException e) {
-			e.printStackTrace();
-		}
+		} 
+//		catch (@SuppressWarnings("restriction") XMLStreamException e) {
+//			e.printStackTrace();
+//		}
 	}
 }
